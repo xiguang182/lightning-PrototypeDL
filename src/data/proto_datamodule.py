aa2f5292d41b156d228c45e99d238ae84783686b
xiguang182 <xiguang182@gmail.com>
2024-07-10 10:48:36 +0900

    model module mostly done, todo add figure logging, add elastic transform to dataloader

---
+++ src/data/proto_datamodule.py
@@ -0,0 +1,288 @@
+from typing import Any, Dict, Optional, Tuple
+
+import torch
+from lightning import LightningDataModule
+from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
+from torchvision.datasets import MNIST
+from torchvision.transforms import transforms
+
+import numpy as np
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+
+
+"""
+Adopted from @author Oscar Li
+
+Source: https://github.com/OscarcarLi/PrototypeDL
+"""
+import numpy as np
+from scipy.ndimage.interpolation import map_coordinates
+from scipy.ndimage.filters import gaussian_filter
+
+def batch_elastic_transform(images, sigma, alpha, height, width, random_state=None):
+    '''
+    this code is borrowed from chsasank on GitHubGist
+    Elastic deformation of images as described in [Simard 2003].
+    
+    images: a two-dimensional numpy array; we can think of it as a list of flattened images
+    sigma: the real-valued variance of the gaussian kernel
+    alpha: a real-value that is multiplied onto the displacement fields
+    
+    returns: an elastically distorted image of the same shape
+    '''
+    assert len(images.shape) == 2
+    # the two lines below ensure we do not alter the array images
+    e_images = np.empty_like(images)
+    e_images[:] = images
+    
+    e_images = e_images.reshape(-1, height, width)
+    
+    if random_state is None:
+        random_state = np.random.RandomState(None)
+    x, y = np.mgrid[0:height, 0:width]
+    
+    for i in range(e_images.shape[0]):
+        
+        dx = gaussian_filter((random_state.rand(height, width) * 2 - 1), sigma, mode='constant') * alpha
+        dy = gaussian_filter((random_state.rand(height, width) * 2 - 1), sigma, mode='constant') * alpha
+        indices = x + dx, y + dy
+        e_images[i] = map_coordinates(e_images[i], indices, order=1)
+
+    return e_images.reshape(-1, height*width)
+
+# function to load and return train and val multi-process iterator over the MNIST dataset.
+
+def get_train_val_loader(data_dir, batch_size, random_seed, augment=False, val_size=0.2, 
+                         shuffle=True, show_sample=False, num_workers=0, pin_memory=True):
+
+    # load the dataset
+    train_dataset = datasets.MNIST(root=data_dir, train=True, 
+                download=True, transform=transforms.ToTensor())
+    val_dataset = datasets.MNIST(root=data_dir, train=True, 
+                download=True, transform=transforms.ToTensor())
+
+    num_train = len(train_dataset)
+    indices = list(range(num_train))
+    split = int(np.floor(val_size * num_train))
+
+    if shuffle == True:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    train_idx, val_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    val_sampler = SubsetRandomSampler(val_idx)
+
+    # create data iterator
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler, 
+                                               num_workers=num_workers, pin_memory=pin_memory)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, sampler=val_sampler, 
+                                             num_workers=num_workers, pin_memory=pin_memory)
+    return (train_loader, val_loader)
+
+# function to load and return a multi-process test iterator over the MNIST dataset.
+def get_test_loader(data_dir, 
+                    batch_size,
+                    shuffle=True,
+                    num_workers=0,
+                    pin_memory=True):
+
+    dataset = datasets.MNIST(root=data_dir, train=False, download=True, transform=transforms.ToTensor())
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, 
+                                              num_workers=num_workers, pin_memory=pin_memory)
+    return data_loader
+
+
+class MNISTDataModule(LightningDataModule):
+    """`LightningDataModule` for the MNIST dataset.
+
+    The MNIST database of handwritten digits has a training set of 60,000 examples, and a test set of 10,000 examples.
+    It is a subset of a larger set available from NIST. The digits have been size-normalized and centered in a
+    fixed-size image. The original black and white images from NIST were size normalized to fit in a 20x20 pixel box
+    while preserving their aspect ratio. The resulting images contain grey levels as a result of the anti-aliasing
+    technique used by the normalization algorithm. the images were centered in a 28x28 image by computing the center of
+    mass of the pixels, and translating the image so as to position this point at the center of the 28x28 field.
+
+    A `LightningDataModule` implements 7 key methods:
+
+    ```python
+        def prepare_data(self):
+        # Things to do on 1 GPU/TPU (not on every GPU/TPU in DDP).
+        # Download data, pre-process, split, save to disk, etc...
+
+        def setup(self, stage):
+        # Things to do on every process in DDP.
+        # Load data, set variables, etc...
+
+        def train_dataloader(self):
+        # return train dataloader
+
+        def val_dataloader(self):
+        # return validation dataloader
+
+        def test_dataloader(self):
+        # return test dataloader
+
+        def predict_dataloader(self):
+        # return predict dataloader
+
+        def teardown(self, stage):
+        # Called on every process in DDP.
+        # Clean up after fit or test.
+    ```
+
+    This allows you to share a full dataset without explaining how to download,
+    split, transform and process the data.
+
+    Read the docs:
+        https://lightning.ai/docs/pytorch/latest/data/datamodule.html
+    """
+
+    def __init__(
+        self,
+        data_dir: str = "data/",
+        train_val_test_split: Tuple[int, int, int] = (55_000, 5_000, 10_000),
+        batch_size: int = 64,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ) -> None:
+        """Initialize a `MNISTDataModule`.
+
+        :param data_dir: The data directory. Defaults to `"data/"`.
+        :param train_val_test_split: The train, validation and test split. Defaults to `(55_000, 5_000, 10_000)`.
+        :param batch_size: The batch size. Defaults to `64`.
+        :param num_workers: The number of workers. Defaults to `0`.
+        :param pin_memory: Whether to pin memory. Defaults to `False`.
+        """
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        # data transformations
+        self.transforms = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        )
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+        self.batch_size_per_device = batch_size
+
+    @property
+    def num_classes(self) -> int:
+        """Get the number of classes.
+
+        :return: The number of MNIST classes (10).
+        """
+        return 10
+
+    def prepare_data(self) -> None:
+        """Download data if needed. Lightning ensures that `self.prepare_data()` is called only
+        within a single process on CPU, so you can safely add your downloading logic within. In
+        case of multi-node training, the execution of this hook depends upon
+        `self.prepare_data_per_node()`.
+
+        Do not use it to assign state (self.x = y).
+        """
+        MNIST(self.hparams.data_dir, train=True, download=True)
+        MNIST(self.hparams.data_dir, train=False, download=True)
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
+
+        This method is called by Lightning before `trainer.fit()`, `trainer.validate()`, `trainer.test()`, and
+        `trainer.predict()`, so be careful not to execute things like random split twice! Also, it is called after
+        `self.prepare_data()` and there is a barrier in between which ensures that all the processes proceed to
+        `self.setup()` once the data is prepared and available for use.
+
+        :param stage: The stage to setup. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`. Defaults to ``None``.
+        """
+        # Divide batch size by the number of devices.
+        if self.trainer is not None:
+            if self.hparams.batch_size % self.trainer.world_size != 0:
+                raise RuntimeError(
+                    f"Batch size ({self.hparams.batch_size}) is not divisible by the number of devices ({self.trainer.world_size})."
+                )
+            self.batch_size_per_device = self.hparams.batch_size // self.trainer.world_size
+
+        # load and split datasets only if not loaded already
+        if not self.data_train and not self.data_val and not self.data_test:
+            trainset = MNIST(self.hparams.data_dir, train=True, transform=self.transforms)
+            testset = MNIST(self.hparams.data_dir, train=False, transform=self.transforms)
+            dataset = ConcatDataset(datasets=[trainset, testset])
+            self.data_train, self.data_val, self.data_test = random_split(
+                dataset=dataset,
+                lengths=self.hparams.train_val_test_split,
+                generator=torch.Generator().manual_seed(42),
+            )
+
+    def train_dataloader(self) -> DataLoader[Any]:
+        """Create and return the train dataloader.
+
+        :return: The train dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> DataLoader[Any]:
+        """Create and return the validation dataloader.
+
+        :return: The validation dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self) -> DataLoader[Any]:
+        """Create and return the test dataloader.
+
+        :return: The test dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def teardown(self, stage: Optional[str] = None) -> None:
+        """Lightning hook for cleaning up after `trainer.fit()`, `trainer.validate()`,
+        `trainer.test()`, and `trainer.predict()`.
+
+        :param stage: The stage being torn down. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`.
+            Defaults to ``None``.
+        """
+        pass
+
+    def state_dict(self) -> Dict[Any, Any]:
+        """Called when saving a checkpoint. Implement to generate and save the datamodule state.
+
+        :return: A dictionary containing the datamodule state that you want to save.
+        """
+        return {}
+
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        """Called when loading a checkpoint. Implement to reload datamodule state given datamodule
+        `state_dict()`.
+
+        :param state_dict: The datamodule state returned by `self.state_dict()`.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    _ = MNISTDataModule()
--- src/models/components/CEA.py
+++ src/models/components/CEA.py
@@ -2,56 +2,13 @@ import os
 import torch
 from torch import nn
 
+from src.utils.proto_utils import list_of_distances
+
 """
 Adopted from @author Oscar Li
 
 Source: https://github.com/OscarcarLi/PrototypeDL
 """
-
-def makedirs(path: str) -> None:
-    """Create a directory if it does not exist.
-
-    :param path: The path to the directory.
-    """
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-def list_of_norms(x: torch.Tensor) -> torch.Tensor:
-    '''
-    Given a list of vectors, X = [x_1, ..., x_n], we return a list of norms
-    [||x_1||, ..., ||x_n||].
-    '''
-    return torch.sum(torch.pow(x, 2), dim = 1)
-    # return torch.sum(x ** 2, -1)
-
-def list_of_distances(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    '''
-    Given a list of vectors, X = [x_1, ..., x_n], and another list of vectors,
-    Y = [y_1, ... , y_m], we return a list of vectors
-            [[d(x_1, y_1), d(x_1, y_2), ... , d(x_1, y_m)],
-             ...
-             [d(x_n, y_1), d(x_n, y_2), ... , d(x_n, y_m)]],
-    where the distance metric used is the sqared euclidean distance.
-    The computation is achieved through a clever use of broadcasting.
-    
-    Two tensors are “broadcastable” if the following rules hold:
-    Each tensor has at least one dimension.
-    When iterating over the dimension sizes, starting at the trailing dimension, the dimension sizes must either be equal, one of them is 1, or one of them does not exist.
-    '''
-    XX = torch.reshape(list_of_norms(x), (-1, 1))
-    YY = torch.reshape(list_of_norms(y), (1, -1))
-    # broadcasting to compute the pairwise squared euclidean distances
-    output = XX - 2 * torch.matmul(x, torch.transpose(y, 0, 1)) + YY 
-    return output
-
-def print_and_write(file, string) -> None:
-    """Print a string to stdout and write it to a file.
-
-    :param file: The file object to write to.
-    :param string: The string to print and write.
-    """
-    print(string)
-    file.write(string + "\n")
 
 class ConvLayer(nn.Module):
     """A convolutional layer for the CEA model."""
@@ -244,7 +201,7 @@ class PrototypeLayer(nn.Module):
         ):
         super().__init__()
 
-        self.prototype_distances = torch.rand((num_prototypes, in_channels), requires_grad=True)
+        self.prototypes = torch.rand((num_prototypes, in_channels), requires_grad=True)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Perform a single forward pass through the network.
@@ -252,7 +209,7 @@ class PrototypeLayer(nn.Module):
         :param x: The input tensor.
         :return: The output tensor.
         """
-        return list_of_distances(x, self.prototype_distances)
+        return list_of_distances(x, self.prototypes)
 
 class CAEModel(nn.Module):
     def __init__(
